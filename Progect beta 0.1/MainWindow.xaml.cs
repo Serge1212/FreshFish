48325314a2c22388cc6ab296d89b1904c25bebf8
sanichace@gmail.com <sanichace@gmail.com>
2020-03-03 17:26:48 +0200

    No Changes

---
+++ Progect beta 0.1/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Progect_beta_0._1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonOpenMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonCloseMenu.Visibility = Visibility.Visible;
+            ButtonOpenMenu.Visibility = Visibility.Collapsed;
+        }
+        private void ButtonCloseMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonCloseMenu.Visibility = Visibility.Collapsed;
+            ButtonOpenMenu.Visibility = Visibility.Visible;
+        }
+        private void ListViewMenu_SelectionChanged (object sender, RoutedEventArgs e)
+        {
+            int index = ListViewMenu.SelectedIndex;
+            MoveCursorMenu(index);
+
+            switch (index)
+            {
+                case 0:
+                    MainFrame.Content = null;
+                    
+                    break;
+                case 1:
+                    MainFrame.Content = null;
+                    MainFrame.Content = new Page1();
+                    break;
+                default:
+                    break;
+            }
+        }
+        private void MoveCursorMenu(int index)
+        {
+            GridCursor.Margin = new Thickness(0, (130 + (50 * index)), 0, 0);
+        }
+    }
+}
